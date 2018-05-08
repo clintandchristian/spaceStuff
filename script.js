c374c96ea1da6b3242df3bc727bc60d75def2405
Christian Fowler <christianhfowler@gmail.com>
2018-05-08 18:07:50 -0400

    js object.key fix THANK YOU RYAN

--- script.js
+++ script.js
@@ -30,14 +30,22 @@ app.getInfo = function() {
       end_date: date()
     }
   }).then(res => {
-    const nearObjects = res.near_earth_objects;
-    console.log(nearObjects);
+        let near = res
+        Object.keys(near.near_earth_objects).forEach(key => {
+              console.log(near.near_earth_objects[key]);
+        })
+        console.log(near)
   });
 };
+
+
 
 app.init = function() {
   app.getInfo();
 };
+
+
+
 
 $(function() {
   app.init();
@@ -45,19 +53,3 @@ $(function() {
 
 //   get current date
 //   get info on all asteroids to pass by today
-//
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth() + 1; //January is 0!
-var yyyy = today.getFullYear();
-
-if (dd < 10) {
-  dd = "0" + dd;
-}
-
-if (mm < 10) {
-  mm = "0" + mm;
-}
-
-today = yyyy + "-" + mm + "-" + dd;
-console.log(today);
