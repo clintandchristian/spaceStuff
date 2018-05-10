2d2e689d86b49ded2ca9e1316d46551c9e845f01
Christian Fowler <christianhfowler@gmail.com>
2018-05-10 16:57:42 -0400

    merge fixed, thanks Ryan

--- script.js
+++ script.js
@@ -1,68 +1,72 @@
 const app = {};
 
 //date function
-const date = function() {
-  var today = new Date();
-  var dd = today.getDate();
-  var mm = today.getMonth() + 1; //January is 0!
-  var yyyy = today.getFullYear();
+const date = function () {
+      var today = new Date();
+      var dd = today.getDate();
+      var mm = today.getMonth() + 1; //January is 0!
+      var yyyy = today.getFullYear();
 
-  if (dd < 10) {
-    dd = "0" + dd;
-  }
+      if (dd < 10) {
+            dd = "0" + dd;
+      }
 
-  if (mm < 10) {
-    mm = "0" + mm;
-  }
+      if (mm < 10) {
+            mm = "0" + mm;
+      }
 
-  today = yyyy + "-" + mm + "-" + dd;
-  return today;
+      today = yyyy + "-" + mm + "-" + dd;
+      return today;
 };
 
 
 const x = date();
 
-app.getInfo = function() {
-  $.ajax({
-    url: "https://api.nasa.gov/neo/rest/v1/feed?",
-    dataType: "json",
-    method: "GET",
-    data: {
-      api_key: "o9VA2EVoF5h978FUEN79Xxi69MSw6jwsUZOFR1VD",
-      // start_date: '2000-01-01'
-      end_date: '2018-05-14'
-    }
-  }).then(res => {
-    console.log(res);
-        Object.keys(res.near_earth_objects).forEach(key => {
-          const arrayOfAsteroids = res.near_earth_objects[key];
-          console.log(arrayOfAsteroids);
-          app.displayInfo(arrayOfAsteroids)
-      //     for(x in arrayOfAsteroids){
-      //           console.log(is_potentially_hazardous_asteroid)
-      //     }
+app.getInfo = function () {
+      $.ajax({
+            url: "https://api.nasa.gov/neo/rest/v1/feed?",
+            dataType: "json",
+            method: "GET",
+            data: {
+                  api_key: "o9VA2EVoF5h978FUEN79Xxi69MSw6jwsUZOFR1VD",
+                  // start_date: '2000-01-01'
+                  end_date: '2018-05-15'
+            }
+      }).then(res => {
+            // console.log(res);
+            Object.keys(res.near_earth_objects).forEach(key => {
+                  const arrayOfAsteroids = res.near_earth_objects[key];
+                  // console.log(arrayOfAsteroids);
+                  app.displayInfo(arrayOfAsteroids)
+                  //     for(x in arrayOfAsteroids){
+                  //           console.log(is_potentially_hazardous_asteroid)
+                  //     }
 
-        })
-  });
+            })
+      });
 };
 avgSize = []
 avgDist = []
 avgSpeed = []
-app.displayInfo = function(asteroids) {
+app.displayInfo = function (asteroids) {
       asteroids.forEach((arr) => {
-            
-            const astName = arr.name.replace(/\s+/g, "");
-            console.log(astName);
+
+            const astName = arr.name.replace(/\s+/g, "").replace(/\(|\)/g, '');
+            // console.log(astName);
             // str.replace(/\s+/g, "");
 
             const astSize = arr.estimated_diameter.kilometers.estimated_diameter_max;
             avgSize.push(astSize)
             // console.log(astSize)
-            
+
             const astDistance = arr.close_approach_data[0].miss_distance.kilometers
+            const astDisToNumber = parseInt(astDistance);
+            avgDist.push(astDisToNumber)
             // console.log(astDistance)
 
             const astSpeed = arr.close_approach_data[0].relative_velocity.kilometers_per_second
+            const astSpeedToNumber = parseInt(astSpeed);
+            avgSpeed.push(astSpeedToNumber)
             // console.log(astSpeed)
 
             const astHazardous = arr.is_potentially_hazardous_asteroid
@@ -84,40 +88,51 @@ app.displayInfo = function(asteroids) {
       })
 
 
-      
-      avgSizeAdded = avgSize.reduce((prev,curr) => {
+
+      avgSizeAdded = avgSize.reduce((prev, curr) => {
             return prev + curr;
       });
       avgSizeTrue = avgSizeAdded / avgSize.length
-      console.log(avgSizeTrue)
+      // console.log(avgSizeTrue)
+
+      avgDistAdded = avgDist.reduce((prev, curr) => {
+            return prev + curr;
+      });
+      avgDistTrue = avgDistAdded / avgDist.length
+      // console.log(avgDistTrue)
+
+      avgSpeedAdded = avgSpeed.reduce((prev, curr) => {
+            return prev + curr;
+      })
+      avgSpeedTrue = avgSpeedAdded / avgSpeed.length
+      // console.log(avgSpeedTrue)
+}
+
+// This function listens to click on unordered list and 
+app.events = function () {
+      $('ul').on('click', 'li', function () {
+            // console.log(e.currentTarget);
+            const selectedAst = $(this).text().replace(/\s+/g, "").replace(/\(|\)/g, '');
+            console.log(selectedAst);
+            $(`.rightSideInfo`).removeClass("active")
+            $(`div.${selectedAst}`).toggleClass("active");
+            // $(`div.${selectedAst}`).toggleClass("")
+
+            // app.individualInfo(selectedAst);
+      })
 }
 
 
-app.events = function() {
-  $('ul').on('click', 'li', function() {
-    // console.log(e.currentTarget);
-        const selectedAst = $(this).text().replace(/\s+/g, "");
-      console.log(selectedAst)
-
-    $("."+selectedAst).addClass("active");
-    // app.individualInfo(selectedAst);
-  })
-}
 
 
-
-
-app.init = function() {
-  app.getInfo();
-  app.events();
+app.init = function () {
+      app.getInfo();
+      app.events();
 };
 
 
 
 
-$(function() {
-  app.init();
+$(function () {
+      app.init();
 });
-
-//   get current date
-//   get info on all asteroids to pass by today
